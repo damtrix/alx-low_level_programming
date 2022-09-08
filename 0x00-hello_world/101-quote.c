d4bb84136f7b624e2c94d9dfeecfd8be7008ad49
damtrix <oludamola.onarinde@gmail.com>
2022-09-08 07:32:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - block
+ *
+ * Description print out character with using printf or puts
+ *
+ * Return:  1
+ */
+
+int main(void)
+{
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(1, quote, 59);
+return (1);
+}
