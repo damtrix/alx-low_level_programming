b5616e2dd627159170508e86ac075e4f1abd0518
damtrix <oludamola.onarinde@gmail.com>
2022-09-09 10:38:17 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print lowercase a to z in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
