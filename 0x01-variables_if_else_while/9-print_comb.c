4e45b1116f4584923f6889fc70779c1517f795fc
damtrix <oludamola.onarinde@gmail.com>
2022-09-09 11:24:50 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,12 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		putchar(a + '0');
 		if (a != 9)
 		{
-			putchar(a + '0');
 			putchar(',');
 			putchar(' ');
 		}
-		else
-			putchar('9');
 	}
 	putchar('\n');
 	return (0);
