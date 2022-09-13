f6afe59ae28a9606fb364013f37dd44a62af004e
damtrix <oludamola.onarinde@gmail.com>
2022-09-13 14:27:48 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry block
+ *
+ * Description: Write a program
+ *              that prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int a = 0, b = 1, next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
