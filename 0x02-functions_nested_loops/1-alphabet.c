eee0af8a609ec6b86e70e455acb3c372ee198425
damtrix <oludamola.onarinde@gmail.com>
2022-09-13 07:22:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: a function that prints the alphabet,
+ *              in lowercase, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
