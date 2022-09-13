af287bc1cbff99ba8f99da1a28e52995f9b84a9c
damtrix <oludamola.onarinde@gmail.com>
2022-09-13 12:26:04 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add: a function that adds two integers and returns the result.
+ *
+ * @a: check input
+ *
+ * @b: check input
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	sum = a + b;
+	return (sum);
+}
