4b83374e44fe43ec892a0a090c8c3426d9daa27f
damtrix <oludamola.onarinde@gmail.com>
2022-09-13 08:00:35 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the
+ *     alphabet, in lowercase, followed by a new line
+ *
+ */
+
+void print_alphabet_X10(void)
+{
+	int i = 0;
+	char ch = 'a';
+
+	while (i < 10)
+	{
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+		i++;
+		putchar('\n');
+	}
+}
