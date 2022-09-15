2e87d9a8c3a9251880544f7f11e0bd5fae8a8eb6
damtrix <oludamola.onarinde@gmail.com>
2022-09-15 15:39:35 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - a program that finds and prints the largest
+ *        prime factor of the number 612852475143,
+ *        followed by a new line.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+
+	unsigned int long n = 612852475143, a = (int)sqrt(n);
+
+	while (1)
+	{
+		if (n % a == 0)
+		{
+			printf("%lu \n", n / a);
+			break;
+		}
+		a--;
+	}
+
+	return (0);
+}
