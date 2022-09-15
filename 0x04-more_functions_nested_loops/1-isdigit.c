cfe4a705ca492984f083dc233ac28f794787fe06
damtrix <oludamola.onarinde@gmail.com>
2022-09-15 05:21:30 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit (0 through 9)
+ *
+ * @c: check the input
+ *
+ * Return: 1 if c is a digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
