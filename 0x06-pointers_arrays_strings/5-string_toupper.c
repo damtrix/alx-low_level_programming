7593a85121aa3a1e8a61641bd8fbd7c31e83f85c
damtrix <oludamola.onarinde@gmail.com>
2022-09-22 01:41:20 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase letters of a string
+ *
+ * @s: Check the input
+ *
+ * Return: 0
+ */
+
+char *string_toupper(char *s)
+{
+	int c;
+	while (s[c] != '\0')
+	{
+		if (s[c] >= 97 && s[c] <= 122)
+		{
+			s[c] = s[c] - 32;
+		}
+		c++;
+	}
+	return (s);
+}
