c66ef97408c3b2950f5065ed3e6e7691fe46a916
damtrix <oludamola.onarinde@gmail.com>
2022-09-28 23:52:01 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,35 @@
 #include "main.h"
 
 /**
- * _sqrt - function to find the square base on half of the n
+ * square - find square root
  *
- * @k: Value of n
+ * @n: int to find square root
  *
- * @l: The half of n
+ * @val: square root
  *
- * Return: answer
+ * Return: int
  */
 
-int _sqrt(int k, int l)
+int square(int n, int val)
 {
-	if (l * l == k)
-		return (l);
-	else if (l * l > k)
-		return (_sqrt(k, l - 1));
+
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
 	else
 		return (-1);
 }
 
 /**
- * _sqrt_recursion - a function that returns
- *                   the natural square root of a number.
+ * _sqrt_recursion - find natural square root
  *
- * @n: Check the input
+ * @n: int
  *
- * Return: Square root
+ * Return: int
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else
-		return (_sqrt(n, n / 2));
+	return (square(n, 1));
 }
