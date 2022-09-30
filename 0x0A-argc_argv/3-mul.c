812b0550fa724acba63a9dafbce8379e2468d4ae
damtrix <oludamola.onarinde@gmail.com>
2022-09-30 12:54:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,11 +14,13 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 	if (argc != 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-
-	return (1);
 }
