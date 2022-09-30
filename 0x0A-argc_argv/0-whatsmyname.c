52abeffff3862c4d47202793e7175e2d09e14720
damtrix <oludamola.onarinde@gmail.com>
2022-09-30 04:50:38 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ *
+ * @argc: count of the arguments supplied to the program.
+ *
+ * @argv: an array of pointers to the strings which are those arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
