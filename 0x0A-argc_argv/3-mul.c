521c2ed3bc2a1fe1f7fe39b3ce9b78f84dd2f205
damtrix <oludamola.onarinde@gmail.com>
2022-09-30 12:41:16 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ *
+ * @argc: count of the arguments supplied to the program.
+ *
+ * @argv: an array of pointers to the strings which are those arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc == 1)
+		printf("Error\n");
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+
+	return (1);
+}
