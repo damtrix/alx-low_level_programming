e65306b70c8c613ed9e5a3a0e1946625e0f43664
damtrix <oludamola.onarinde@gmail.com>
2022-10-04 02:39:41 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,11 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *arr = malloc(size);
+	char *arr = malloc(size);
 
-    if (size == 0 || arr == 0)
-        return (0);
-    while (size--)
-        arr[size] = c;
-
-    return (arr);
+	if (size == 0 || arr == 0)
+		return (0);
+	while (size--)
+		arr[size] = c;
+	return (arr);
 }
