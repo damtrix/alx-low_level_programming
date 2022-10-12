057d4e407d6d69632d8ee40b14d7a173dd2bd5ba
damtrix <oludamola.onarinde@gmail.com>
2022-10-12 11:04:00 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,9 @@
  * int_index - a function that searches for an integer
  *
  * @array: pointer to array
- * @size: size of @array
+ *
+ * @size: size of array
+ *
  * @cmp: pointer to function call to check index in array
  *       if it matches
  *
@@ -13,22 +15,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
-	bool y;
+	int i;
 
-	if (array != NULL && size > 0 && cmp != NULL)
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
-
-		for (index = 0; index < size; index++)
-		{
-			y = cmp(array[index]);
-			if (y == TRUE)
-				return (index);
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
 
 	return (-1);
-
 }
