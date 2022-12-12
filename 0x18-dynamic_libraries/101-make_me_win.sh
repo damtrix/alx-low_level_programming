3be281b59f82f71836cec65360bb5b43f21d257f
damtrix <oludamola.onarinde@gmail.com>
2022-12-12 13:09:06 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/damtrix/alx-low_level_programming/raw/master/0x18-dynamic_libraries/randnum.so
+wget -P /tmp https://raw.github.com/damtrix/alx-low_level_programming/master/0x18-dynamic_libraries/randnum.so
 export LD_PRELOAD=/tmp/randnum.so
